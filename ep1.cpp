5f71f96ba893f04c23918ed716e69ce02e73c6da
Yotsaphon <jew2121@gmail.com>
2019-01-17 01:21:40 +0700

    ver1

---
+++ ep1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void reverseString(string x){
+  string y=x;
+  int L=x.size();
+
+  int i=0;
+  while (i<L)
+  {
+    std::cout << x[L-i-1] ;
+    i++;
+  }
+
+}
+int main(){
+  string s;
+  std::cout << "Enter string : ";
+  std::cin >> s;
+  reverseString(s);
+return 0;
+}
